f3dea474bdd33b94d034bd18a1b3346a33beb144
rakib <rakibco66@gmail.com>
2026-02-03 21:35:51 +0600

    learn while loop

---
+++ while.js
@@ -0,0 +1,15 @@
+const number = 7;
+
+if (number < 10){
+    console.log("choto akta number paisi")
+}
+
+console.log("somethisng new is cooking ")
+
+
+let num = 0;
+while(num < 5) {
+  
+    console.log("loooooing")
+    num = num + 1;
+}
