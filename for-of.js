1e7e9767f8e7cf95401e4d664ee9fc9f5068ec2e
rakib <rakibco66@gmail.com>
2026-02-03 21:04:05 +0600

    learn for of loop

---
+++ for-of.js
@@ -0,0 +1,15 @@
+const numbers = [12, 44, 55, 34,50]
+
+console.log(numbers)
+
+for(const num of numbers) {
+    console.log(num)
+}
+
+
+const fruits = ["orange", 'apple', 'bannana', 'mango'];
+
+for(const fruit of fruits) {
+    console.log(fruit)
+    console.log("I wnat to eat")
+}
