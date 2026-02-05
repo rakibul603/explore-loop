1254728ca27d0d22ce9228d34db518185de97be1
rakib <rakibco66@gmail.com>
2026-02-05 21:24:01 +0600

    learn continue in loop

---
+++ continue.js
@@ -0,0 +1,17 @@
+// for (let i = 1; i < 10; i ++) {
+//     if (i % 2 === 1) {
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+let n = 1;
+
+while(n < 10) {
+    n++;
+    if(n % 2 === 1) {
+        continue;
+    }
+
+    console.log(n)
+}
