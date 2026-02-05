7cd8cf3ad9fd91852cf3d452b886bea9928aa7b8
rakib <rakibco66@gmail.com>
2026-02-05 21:12:02 +0600

    learn break

---
+++ brek.js
@@ -0,0 +1,16 @@
+// for(let i = 0; i <= 10; i++) {
+//     console.log(i)
+//     if(i >= 5) {
+//         break;
+//     }
+//     console.log('life after is better')
+// }
+
+
+let n = 64;
+
+while(n > 50) {
+    console.log(n)
+    n--;
+    break;
+}
