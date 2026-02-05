720be95c80d3ee69f301920982e61eef76040aab
rakib <rakibco66@gmail.com>
2026-02-05 18:26:32 +0600

    learn and explore loop

---
+++ for-sum.js
@@ -0,0 +1,11 @@
+
+let sum = 0;
+
+
+for (let i = 11; i <= 20; i++) {
+    sum += i;
+
+    console.log(i)
+}
+
+console.log('sum 11 to 20 is ', sum)
---
+++ for.js
@@ -0,0 +1,10 @@
+// let num = 1;
+
+// while (num <= 10) {
+//     console.log(num)
+//     num++
+// }
+
+for (let i = 0 ; i <= 5; i++) {
+    console.log(i)
+}
